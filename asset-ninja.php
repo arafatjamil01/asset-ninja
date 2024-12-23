23693e9d8f5c67717aa05294ad2aee981dd0d85d
arafatjamil01 <arafatjamil01@gmail.com>
2024-12-23 12:39:35 +0600

    Loading admin assets, and working with current screen, post type

--- asset-ninja.php
+++ asset-ninja.php
@@ -20,6 +20,17 @@ class AssetNinja {
 	public function __construct() {
 		add_action( 'init', array( $this, 'load_textdomain' ) );
 		add_action( 'wp_enqueue_scripts', array( $this, 'load_front_assets' ) );
+		add_action( 'admin_enqueue_scripts', array( $this, 'load_admin_assets' ) );
+	}
+
+	public function load_admin_assets( $screen ) {
+		$current_screen = get_current_screen();
+
+		// if( 'edit.php' == $screen && 'page' == $current_screen->post_type){
+		if ( 'edit.php' == $current_screen->base && 'page' == $current_screen->post_type ) {
+			wp_enqueue_script( 'asn-admin-script', ASN_ASSETS_ADMIN_DIR . '/js/main.js', array( 'jquery' ), ASN_VERSION, true );
+			wp_enqueue_style( 'asn-admin-style', ASN_ASSETS_ADMIN_DIR . '/css/style.css', array(), ASN_VERSION, 'all' );
+		}
 	}
 
 	public function load_front_assets() {
@@ -28,13 +39,13 @@ class AssetNinja {
 		wp_enqueue_script( 'asn-another', ASN_ASSETS_PUBLIC_DIR . '/js/another.js', array( 'jquery' ), ASN_VERSION, true );
 
 		// NOTE: You can't have the third script depend on the first script again -> circular dependency. 3rd script can depend on the 2nd script. 2nd can depend on the 1st script. but 1st can't depend on the 3rd script.
-		// This will create an infinite loop, the software will crash. 
+		// This will create an infinite loop, the software will crash.
 
 		// Passing data to JS from php
-		$data = [
-			'name' => "Arafat Jamil",
-			'github' => "https://github.com/arafatjamil01"
-		];
+		$data = array(
+			'name'   => 'Arafat Jamil',
+			'github' => 'https://github.com/arafatjamil01',
+		);
 
 		wp_localize_script( 'asn-script', 'araf', $data ); // araf is the name of the object you can define yourself.
 	}
